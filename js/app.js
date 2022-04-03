bd6ed0890e25db0c379eb227caeccfe19220fc24
Mortuz A <mortuz@idevia.in>
2022-04-03 05:56:28 +0530

    dynamic quotes

--- js/app.js
+++ js/app.js
@@ -4,3 +4,20 @@ document.querySelectorAll('.color').forEach(function (c) {
     document.querySelector('body').style.backgroundColor = color
   })
 })
+
+fetchQuote = (id) => {
+  fetch('https://api.quotable.io/random')
+    .then(data => data.json())
+    .then(data => {
+      console.log(document.querySelector(`.quote-${id}`))
+      document.querySelector(`.quote-${id} .js-quote`).innerHTML = data.content
+      document.querySelector(`.quote-${id} .js-author`).innerHTML = data.author
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+fetchQuote(1)
+fetchQuote(2)
+fetchQuote(3)
