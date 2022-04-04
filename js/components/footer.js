8bf6360e91a01998afaa6d7620e32565f7571ad8
Mortuz A <mortuz@idevia.in>
2022-04-04 12:57:33 +0530

    nav and footer component, dynamic button colors, dynamic svg color

---
+++ js/components/footer.js
@@ -0,0 +1,27 @@
+document.write(`
+<footer class="navbar navbar-expand-lg navbar-light d-flex align-items-center" style="z-index: 2">
+      <div class="container-fluid">
+      <div class="popup-container position-relative">
+        <a class="small-font white-color" style="text-decoration: none" href="#!">About liquote</a>
+        <div class="popup position-absolute">
+          <p>What makes quotes even better? Quotes with minimilistic style. </p>
+          <p>Liquote is free and for everyone. Our quotes are to motivate, inspire and inform everyone around the world. </p>
+          <p>© Liquote by <a href="https://idevia.in" target="_blank">iDevia</a>. From a creative to all the others. </p>
+        </div>
+      </div>
+
+        <div class="justify-content-end">
+          <div class="color-options">
+            <span class="color black" data-name="black" data-color="#262D33"></span>
+            <span class="color red" data-name="red" data-color="#FF4F52"></span>
+            <span class="color green" data-name="green" data-color="#3DC47E"></span>
+            <span class="color han-blue" data-name="han-blue" data-color="#4267B2"></span>
+            <span class="color sky-blue" data-name="sky-blue" data-color="#4CB9FC"></span>
+            <span class="color purple" data-name="purple" data-color="#6D75E7"></span>
+            <span class="color yellow" data-name="yellow" data-color="#FFD980"></span>
+            <span class="color blue" data-name="blue" data-color="#3587FF"></span>
+          </div>
+        </div>
+      </div>
+    </footer>
+`)
